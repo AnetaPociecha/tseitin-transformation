2accd1b477948f719c9da6e6bb7a0e46f6f20504
Aneta <anpociecha@gmail.com>
2020-07-13 18:14:55 +0200

    update mock cnf file initialization

--- src/main/java/pl/edu/agh/TseitinTransformation.java
+++ src/main/java/pl/edu/agh/TseitinTransformation.java
@@ -1,5 +1,7 @@
 package pl.edu.agh;
 
+import java.io.IOException;
+
 public class TseitinTransformation {
 
     private Formula formula;
@@ -8,9 +10,9 @@ public class TseitinTransformation {
         this.formula = new Formula(dnfFile);
     }
 
-    public CNFFile convertDNFToCNF() {
+    public CNFFile convertDNFToCNF() throws IOException {
         CNFFile.CNFFileBuilder cnfFileBuilder = new CNFFile.CNFFileBuilder();
-        CNFFile cnfFile = cnfFileBuilder.filepath("").build();
+        CNFFile cnfFile = cnfFileBuilder.build();
         return cnfFile;
     }
 
